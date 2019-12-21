f84b89288a97e9826a705275b00b9221f2d98bf9
Talita <silveira.tacristina@gmail.com>
2019-12-21 18:17:26 -0300

    update meetup

--- src/app/controllers/MeetupController/MeetupController.js
+++ src/app/controllers/MeetupController/MeetupController.js
@@ -1,9 +1,9 @@
+import * as yup from 'yup';
+import dayjs from 'dayjs';
 import Meetup from '../../models/Meetup';
 import Game from '../../models/Game';
 import Player from '../../models/Player';
 import MeetupPlayer from '../../models/MeetupPlayer';
-import * as yup from 'yup';
-import dayjs from 'dayjs';
 
 class MeetupController {
   async createMeetup(req, res) {
@@ -11,8 +11,12 @@ class MeetupController {
     const { date, name } = req.body;
 
     const dayjs = require('dayjs');
-    let dateToFormat = dayjs(date);
+
+    const dateToFormat = dayjs(date, 'America/Sao_Paulo');
     req.body.date = dateToFormat.format('YYYY-MM-DDTHH:mm:ssZ');
+
+    // const hour = dateToFormat.get('hour');
+    // const minute = dateToFormat.get('minute');
 
     const meetup = {
       name,
@@ -32,19 +36,59 @@ class MeetupController {
     }
 
     try {
-      const gameExists = await Game.findOne({ where: { id: gameId } });
+      const gameExists = await Game.findOne({
+        where: { id: gameId },
+      });
       if (gameExists) {
-
         const meetupCreated = await Meetup.create(meetup);
         return res.send(meetupCreated);
       }
       return res.json({ error: 'Game Not Found' });
-
     } catch (err) {
       console.error(err);
     }
   }
+
+  async updateMeetup(req, res) {
+    const { date, isCanceled } = req.body;
+    const { meetupId } = req.params;
+
+    const dayjs = require('dayjs');
+    const dateToFormat = dayjs(date, 'America/Sao_Paulo');
+    req.body.date = dateToFormat.format('YYYY-MM-DDTHH:mm:ssZ');
+
+    const meetupToChange = {
+      isCanceled,
+      date,
+    };
+
+    const yup = require('yup');
+    const schema = yup.object().shape({
+      isCanceled: yup.boolean(),
+      date: yup.date().required(),
+    });
+
+    if (!(await schema.isValid(meetupToChange))) {
+      return res.status(400).json({ error: 'Validation Fail' });
+    }
+    try {
+      const meetupExists = await Meetup.findOne({ where: { id: meetupId } });
+      if (meetupExists) {
+        const meetupChange = await Meetup.update(meetupToChange, {
+          where: { id: meetupId },
+        });
+        if (meetupChange !== undefined) {
+          const returnChanged = await Meetup.findOne({
+            where: { id: meetupId },
+          });
+          return res.send(returnChanged);
+        }
+
+        return res.json({ error: 'error now' });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
-
-
 export default new MeetupController();
--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,20 @@ import Player from './app/models/Player.js';
 import PlayerController from './app/controllers/PlayerController/PlayerController.js';
 import MeetupPlayer from './app/models/MeetupPlayer';
 import MeetupPlayerController from './app/controllers/MeetupPlayer/MeetupPlayerController';
+import Meetup from './app/models/Meetup';
+
 const routes = new Router();
 
-routes.post("/games", GameController.store);
-routes.patch("/games/:id", GameController.updateGame);
+routes.post('/games', GameController.store);
+routes.patch('/games/:id', GameController.updateGame);
 routes.delete('/games/:id', GameController.deleteGame);
 routes.get('/games/', GameController.searchGames);
 
 routes.post('/games/:gameId/meetup', MeetupController.createMeetup);
 routes.post('/players', PlayerController.createPlayer);
-routes.post('/invite/player/:playerId/meetup/:meetupId/game/:gameId', MeetupPlayerController.invitePlayerToMeetup);
-
+routes.post(
+  '/invite/player/:playerId/meetup/:meetupId/game/:gameId',
+  MeetupPlayerController.invitePlayerToMeetup
+);
+routes.put('/meetup/:meetupId/', MeetupController.updateMeetup);
 export default routes;

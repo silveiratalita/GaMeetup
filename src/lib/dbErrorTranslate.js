96e1500151128cd4919c600a93aa6fa0492e99e2
Talita <silveira.tacristina@gmail.com>
2020-01-27 21:22:16 -0300

    tratando erros

--- src/lib/dbErrorTranslate.js
+++ src/lib/dbErrorTranslate.js
@@ -1,15 +1,14 @@
 function dbErrorTranslate(err) {
   switch (err.code) {
     case '23502':
-      return `Você caiu no erro  ${err.code}! A mensagem que foi informada é a   ${err.detail}Esse erro ocorreu na tabela ${err.table}. O erro foi verificado no processo ${err.routine}. Portanto, por favor, verifique  a constraint ${err.constraint}, já que há grande chance de esse campo não poder ser nulo ou undefined, logo, você não poderá inserir valores nulos ou undefined nele!`;
+      return `You are in  ${err.code}error code! The message is   ${err.detail}. This errror occurred in  ${err.table} table.  This erros was checked in routine ${err.routine}. So, please, check the constraint ${err.constraint},since there is a big chance that this field cannot be null, therefore, you cannot insert null or undefined values in it!!!`;
       break;
     case '23505':
-      return `Você caiu no erro  ${err.code}! A mensagem que foi informada é a   ${err.detail}Esse erro ocorreu na tabela ${err.table}. O erro foi verificado no processo ${err.routine}. Portanto, por favor, verifique  a constraint ${err.constraint}, já que há grande chance de esse campo ser unique e não poder ser repetido em outro registro.  `;
+      return `You are in  ${err.code}! The message is   ${err.detail}. This errror occurred in  ${err.table} table.  This erros was checked in routine ${err.routine}. So, please, check the constraint ${err.constraint},since there is a big chance that this field be a unique key, and you can't  repeat this value.  `;
       break;
     default:
-      return 'error:500';
+      return 'error: 500. Something went wrong!';
   }
-  console.log('aqiooooo', err);
 }
 
 export default dbErrorTranslate;

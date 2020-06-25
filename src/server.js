d86712c3cd943b6fea10fd1073a6235bde6102e1
Talita Silveira <talita.silveira@kroton.com.br>
2020-06-25 17:15:26 -0300

    .

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(3333, console.log('Death By Unga Bunga - I Cant Believe That Were Together => https://open.spotify.com/track/3gRLnHzO4GLhe84FcAMa0d?si=dD8v-mPYRlqtK7AvvjuS5w'));
+app.listen(3333, console.log('Death By Unga Bunga -I Cant Believe That Were Together => https://open.spotify.com/track/3gRLnHzO4GLhe84FcAMa0d?si=dD8v-mPYRlqtK7AvvjuS5w'));
